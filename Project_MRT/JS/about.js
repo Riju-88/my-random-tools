97e9151709d1c721783440fd6cec32ee9a9bd4ce
Riju-88 <90795188+Riju-88@users.noreply.github.com>
2021-10-19 02:20:52 +0530

    Delete Project_MRT directory

--- Project_MRT/JS/about.js
+++
@@ -1,7 +0,0 @@
-let fb= document.getElementById("fb");
- fb.addEventListener("click",()=>{
-    fb.href="https://www.facebook.com/uxplayer01";
-    setTimeout(() => {
-        fb.href="";
-    }, 100);
-})
